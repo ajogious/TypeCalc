8f9b40422dc3757b7aa03552922cfc08818bf45c
ajogious <ajogious@gmail.com>
2025-07-27 14:55:44 +0100

    Updated

---
+++ src/script.ts
@@ -0,0 +1,6 @@
+// <========== TypeScript ==========> //
+function calculate(operation: "add" | "subtract" | "multiply" | "divid"): void {
+  const num1Input = document.getElementById("num1") as HTMLInputElement;
+  const num2Input = document.getElementById("num2") as HTMLInputElement;
+  const resultDiv = document.getElementById("result") as HTMLInputElement;
+}
