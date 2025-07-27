3d5a3cadf83acbca440bafe0aa7f9e1b794a34e4
ajogious <ajogious@gmail.com>
2025-07-27 15:38:28 +0100

    working code

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,47 @@
-// <========== TypeScript ==========> //
-function calculate(operation: "add" | "subtract" | "multiply" | "divid"): void {
+// <========== CALCULATION FUNCTION ==========> //
+function calculate(
+  operation: "add" | "subtract" | "multiply" | "divide"
+): void {
   const num1Input = document.getElementById("num1") as HTMLInputElement;
   const num2Input = document.getElementById("num2") as HTMLInputElement;
-  const resultDiv = document.getElementById("result") as HTMLInputElement;
+  const resultDiv = document.getElementById("result") as HTMLElement;
+
+  const num1: number = parseFloat(num1Input.value);
+  const num2: number = parseFloat(num2Input.value);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    alert("Please enter valid numbers!");
+    return;
+  }
+
+  let result: number;
+
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      if (num2 === 0) {
+        alert("Divison by zero(0) not valid!");
+        return;
+      }
+      result = num1 / num2;
+      break;
+  }
+
+  resultDiv.textContent = `Result: ${result}`;
+}
+
+// <========== RESET FUNCTION ==========> //
+function resetForm(): void {
+  (document.getElementById("num1") as HTMLInputElement).value = "";
+  (document.getElementById("num2") as HTMLInputElement).value = "";
+  const resultDiv = document.getElementById("result") as HTMLElement;
+  resultDiv.textContent = "Result: ";
 }
